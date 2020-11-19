702f0b76a8a9ebbd348a5512d5e4ddeb61c63b3b
nazmul199512 <nazmul199512@gmail.com>
2020-11-19 13:20:46 +0600

    fixed cores models

--- core/models.py
+++ core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# SEARCH MODEL
+
 class Search(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     keyword = models.CharField(max_length=10)
