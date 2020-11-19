eeea90dc3f0abc36e63496d867c5e5bb838868ae
nazmul199512 <nazmul199512@gmail.com>
2020-11-19 12:22:28 +0600

    authentication app added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Search(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    keyword = models.CharField(max_length=10)
+    search_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} -> {}'.format(self.user.username, self.keyword[:30])
+
+    class Meta:
+        verbose_name_plural = 'search histories'
